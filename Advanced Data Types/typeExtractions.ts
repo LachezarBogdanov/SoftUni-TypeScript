b3b3252e222b4cae2be95d5de140c5692726e1c9
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2025-06-01 23:37:37 +0300

    Added 07 ex

--- Advanced Data Types/typeExtractions.ts
+++ Advanced Data Types/typeExtractions.ts
@@ -1,18 +1,18 @@
 
-let names = { fName: 'John', lName: 'Doe', age: 22, getPersonInfo() { return `${this.fName} ${this.lName}, age ${this.age}` } };
-let location = { city:'Boston', street: 'Nowhere street', number: 13, postalCode: 51225, getAddressInfo() { return `${this.street} ${this.number}, ${this.city} ${this.postalCode}`} };
+// let names = { fName: 'John', lName: 'Doe', age: 22, getPersonInfo() { return `${this.fName} ${this.lName}, age ${this.age}` } };
+// // let location = { city:'Boston', street: 'Nowhere street', number: 13, postalCode: 51225, getAddressInfo() { return `${this.street} ${this.number}, ${this.city} ${this.postalCode}`} };
 
 
-type namesType = typeof names;
-type locationType = typeof location;
+// type namesType = typeof names;
+// type locationType = typeof location;
 
-function createCombinedFunction(name: namesType, location: locationType) {
-    return function(combinedObject: namesType & locationType) {
-        console.log(`Hello, ${combinedObject.getPersonInfo()} from ${combinedObject.getAddressInfo()}`);
+// function createCombinedFunction(name: namesType, location: locationType) {
+//     return function(combinedObject: namesType & locationType) {
+//         console.log(`Hello, ${combinedObject.getPersonInfo()} from ${combinedObject.getAddressInfo()}`);
         
-    }
-}
+//     }
+// }
 
-let combinedFunction = createCombinedFunction(names, location); 
-let combinedPerson = Object.assign({}, names, location); 
-combinedFunction(combinedPerson);
+// let combinedFunction = createCombinedFunction(names, location); 
+// let combinedPerson = Object.assign({}, names, location); 
+// combinedFunction(combinedPerson);
