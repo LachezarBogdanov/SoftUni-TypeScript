17faad36a874d47d0917e1e4cee522680600159f
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2025-06-07 16:55:56 +0300

    Added exercises from mastering generics

---
+++ Exercise Mastering Generics/conditionalNumber.ts
@@ -0,0 +1,15 @@
+type CheckType<T> = T extends number ? number : string;
+
+function conditionalNumber<T>(param: CheckType<T>) {
+    if(typeof param === 'number') {
+        console.log(param.toFixed(2));
+    } else {
+        console.log(param);
+    }
+}
+
+conditionalNumber<number>(20.3555);
+
+conditionalNumber<string>('wow');
+
+conditionalNumber<boolean>('a string');
